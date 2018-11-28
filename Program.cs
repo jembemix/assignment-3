635eda8ee8c2feb09f3b03b78b52d6332413921d
samso <samso@DESKTOP-N8052KE>
2018-11-27 18:00:56 -0800

    fixed

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GettingSchooled
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Instructor John = new Instructor("John", "English");
+            Instructor Mike = new Instructor("Mike", "Math");
+            Student Jane = new Student("Jane", John);
+            Student Joe = new Student("Joe", John);
+            Student Melissa = new Student("Melissa", Mike);
+            Student Matt = new Student("Matt", Mike);
+            John.SetStudentGrade(Jane, 95);
+            John.SetStudentGrade(Joe, 85);
+            Mike.SetStudentGrade(Melissa, 90);
+            Mike.SetStudentGrade(Matt, 92);
+            Jane.StudentPrint();
+            Joe.StudentPrint();
+            Melissa.StudentPrint();
+            Matt.StudentPrint();
+            System.Console.ReadKey();
+        }
+    }
+}
